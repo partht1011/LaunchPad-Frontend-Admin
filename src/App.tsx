a198fe10522dd441c3a2ec01132118ad2f05a36d
snow <partht1011@gmail.com>
2024-11-14 18:57:28 -0800

    init infrastructure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './assets/svgs/logo.svg';
+import './styles/App.css';
+
+function App() {
+  return (
+    <div className="">
+    </div>
+  );
+}
+
+export default App;
